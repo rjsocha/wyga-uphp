989c4608a4da0cb84497c5e79b98e3fa20a06999
Robert Socha <socha@socha.it>
2024-08-29 02:31:38 +0200

    uPHP

--- context/container-config.sh
+++ context/container-config.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 set -eu
 umask 0022
-printf -- "1: HOME CONFIG\n"
 home-config
-printf -- "2: CONFIGUS CONFIG\n"
 configus-config
-printf -- "3: EXTENSION CONFIG\n"
 extension-config
-printf -- "4: TIMEZONE CONFIG\n"
 timezone-config
-printf -- "5: XDEBUG CONFIG\n"
 xdebug-config
-printf -- "0: DONE\n"
